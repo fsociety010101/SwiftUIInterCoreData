7ba1f1c7ad58d12a247fafa04d871fd822bbe461
Dmytro Nimchynskyi <nimchynskyi@icloud.com>
2023-11-11 18:21:12 +0100

    Initial Commit

---
+++ SwiftUIInterCoreData/SwiftUIInterCoreDataApp.swift
@@ -0,0 +1,20 @@
+//
+//  SwiftUIInterCoreDataApp.swift
+//  SwiftUIInterCoreData
+//
+//  Created by Dmytro Nimchynskyi on 11/11/2023.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUIInterCoreDataApp: App {
+    let persistenceController = PersistenceController.shared
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+                .environment(\.managedObjectContext, persistenceController.container.viewContext)
+        }
+    }
+}
